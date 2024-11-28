d78442a9dcd1389dcfea0fe2b72eb4e0636d0281
Alejandro <abellol@unal.edu.co>
2024-11-28 09:52:34 -0500

    corrected_file

--- hackerrank1.py
+++ hackerrank1.py
@@ -2,29 +2,35 @@ class Complex(object):
     def __init__(self, real, imaginary):
         self.real=real
         self.imaginary=imaginary
+
     def __add__(self, no:"Complex"):
         sum_imaginary = self.imaginary + no.imaginary
         sum_real = self.real + no.real
         sum=Complex(real=sum_real, imaginary=sum_imaginary)
         return sum
+    
     def __sub__(self, no:"Complex"):
         sub_imaginary = self.imaginary - no.imaginary
         sub_real = self.real - no.real
         sub=Complex(real=sub_real, imaginary=sub_imaginary)
         return sub
+    
     def __mul__(self, no:"Complex"):
         mul_real= (self.real*no.real)-(self.imaginary*no.imaginary)
         mul_imaginary=(self.real*no.imaginary)+(self.imaginary*no.real)
         mul=Complex(real=mul_real, imaginary=mul_imaginary)
         return mul
+    
     def __truediv__(self, no:"Complex"):
         div_real= ((self.real*no.real)+(self.imaginary*no.imaginary))/((no.real**2)+(no.imaginary**2))
         div_imaginary=((self.imaginary*no.real)-(self.real*no.imaginary))/((no.real**2)+(no.imaginary**2))
         div=Complex(real=div_real, imaginary=div_imaginary)
         return div
+    
     def mod(self):
         mod=((self.real**2)+(self.imaginary**2))**(1/2)
         return Complex(real=mod, imaginary=0)
+    
     def __str__(self):
         if self.imaginary == 0:
             result = "%.2f+0.00i" % (self.real)
